951c8eb7a7f51c9b5f48956d546513d98297d8e9
tu.lt.01 <carlallen2904@gmail.com>
2018-05-06 11:24:36 +0700

    Add library

--- app/src/main/java/com/example/anhtu/atstore/MainActivity.java
+++ app/src/main/java/com/example/anhtu/atstore/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.anhtu.atstore;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewmanhinhchinh;
+    NavigationView navigationView;
+    ListView listViewmanhinhchinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+    }
+
+    private void AnhXa() {
+        toolbar = (Toolbar) findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewlipper);
+        recyclerViewmanhinhchinh = (RecyclerView) findViewById(R.id.recyclerview);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        listViewmanhinhchinh = (ListView) findViewById(R.id.listviewmanhinhchinh);
     }
 }
