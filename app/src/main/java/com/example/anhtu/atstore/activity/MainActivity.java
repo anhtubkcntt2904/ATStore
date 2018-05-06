16ad90373957beb4bccb5c22d20897962ef32915
tu.lt.01 <carlallen2904@gmail.com>
2018-05-06 14:09:20 +0700

    Add library

--- app/src/main/java/com/example/anhtu/atstore/activity/MainActivity.java
+++ app/src/main/java/com/example/anhtu/atstore/activity/MainActivity.java
@@ -1,12 +1,19 @@
-package com.example.anhtu.atstore;
+package com.example.anhtu.atstore.activity;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
-import android.widget.Toolbar;
 import android.widget.ViewFlipper;
+
+import com.example.anhtu.atstore.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +22,34 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerViewmanhinhchinh;
     NavigationView navigationView;
     ListView listViewmanhinhchinh;
-
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AnhXa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     private void AnhXa() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout); 
         toolbar = (Toolbar) findViewById(R.id.toolbarmanhinhchinh);
         viewFlipper = (ViewFlipper) findViewById(R.id.viewlipper);
         recyclerViewmanhinhchinh = (RecyclerView) findViewById(R.id.recyclerview);
