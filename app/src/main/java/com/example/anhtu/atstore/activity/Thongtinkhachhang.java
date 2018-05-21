72c31c48841757802d07d58b6648e1b42317e073
tu.lt.01 <carlallen2904@gmail.com>
2018-05-22 03:58:35 +0700

    chap 29 316

--- app/src/main/java/com/example/anhtu/atstore/activity/Thongtinkhachhang.java
+++ app/src/main/java/com/example/anhtu/atstore/activity/Thongtinkhachhang.java
@@ -2,14 +2,93 @@ package com.example.anhtu.atstore.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.anhtu.atstore.R;
+import com.example.anhtu.atstore.ultil.CheckConnection;
+import com.example.anhtu.atstore.ultil.Server;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Thongtinkhachhang extends AppCompatActivity {
+
+    EditText edttenkhachhang, edtemail, edtsdt;
+    Button btnxacnhan, btntrove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongtinkhachhang);
+        AnhXa();
+        btntrove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        if (CheckConnection.haveNetworkConnection(getApplicationContext())) {
+            EventButton();
+        } else {
+            CheckConnection.showToast_Short(getApplicationContext(), "Check your connection");
+        }
+    }
+
+    //bắt sự kiện click xác nhận
+    private void EventButton() {
+        btnxacnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String ten = edttenkhachhang.getText().toString().trim();
+                final String sdt = edtsdt.getText().toString().trim();
+                final String email = edtemail.getText().toString().trim();
+                if (ten.length() > 0 && sdt.length() > 0 && email.length() > 0) {
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    //gửi lên server dưới dạng string request
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, Server.Duongdandonhang, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("madonhang", response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("tenkhachhang", ten);
+                            hashMap.put("sodienthoai", sdt);
+                            hashMap.put("email", email);
+                            return hashMap;
+                        }
+                    };
+                    requestQueue.add(stringRequest);
+                } else {
+                    CheckConnection.showToast_Short(getApplicationContext(), "Check your data again");
+                }
+            }
+        });
+    }
+
+    private void AnhXa() {
+        edttenkhachhang = findViewById(R.id.edittexttenkhachhang);
+        edtemail = findViewById(R.id.edittextemail);
+        edtsdt = findViewById(R.id.edittextsodienthoai);
+        btnxacnhan = findViewById(R.id.buttonxacnhan);
+        btntrove = findViewById(R.id.buttontrove);
     }
 }
--- app/src/main/java/com/example/anhtu/atstore/ultil/Server.java
+++ app/src/main/java/com/example/anhtu/atstore/ultil/Server.java
@@ -9,4 +9,5 @@ public class Server {
     public static String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
     public static String Duongdansanphammoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
     public static String Duongdandienthoai = "http://" + localhost + "/server/getsanpham.php?page=";
+    public static String Duongdandonhang = "http://" + localhost + "/server/thongtinkhachhang.php";
 }
